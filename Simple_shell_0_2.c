1a74d57f22dda34f494e75acd44a199f3c69e1eb
SirMphuthi <kgotsomphuthi@gmail.com>
2024-08-26 11:08:15 -0700

    handles

--- Simple_shell_0_2.c
+++ Simple_shell_0_2.c
@@ -5,69 +5,66 @@
 #include <string.h>
 
 /**
- * interpreter - prompts a user to enter a command with arguments and executes it.
- * Return: 0 when done.
+ * interpreter - prompts a user to enter a command with arguments and executes
+ * Return: 0 when done
  */
 
 int interpreter(void)
 {
-    char *line = NULL;
-    size_t length = 0;
-    ssize_t reader;
-    char *args[8];
-    int x;
-    pid_t pid;
-    int status;
+	char *line = NULL;
+	size_t length = 0;
+	ssize_t reader;
+	char *args[8];
+	int x;
+	pid_t pid;
+	int status;
 
-    while (1)
-    {
-        write(STDOUT_FILENO, " # ", 3); 
+	while (1)
+	{
+		while(STDOUT_FILENO, " # ", 3);
 
-        reader = getline(&line, &length, stdin);
-        if (reader == -1)  // Handle EOF (Ctrl+D)
-        {
-            write(STDOUT_FILENO, "\n", 1);
-            break;
-        }
+		reader = getline(&line, &length, stdin);
+		if (reader == -1)
+		{
+			write(STDOUT_FILENO, "\n");
+			break;
+		}
 
-        // Tokenize input line into command and arguments
-        x = 0;
-        args[x] = strtok(line, " \n");
-        if (args[x] == NULL)  // No input, just press Enter
-        {
-            continue;
-        }
-        while (args[x] != NULL && x < 7)
-        {
-            x++;
-            args[x] = strtok(NULL, " \n");
-        }
-        args[x] = NULL;
+		x = 0;
+		args[x] = strtok(line,"\n");
+		if (args[x] == NULL)
+		{
+			continue;
+		}
+		while (args[x] != NULL && x < 7)
+		{
+			x++;
+			args[x] = strtok(NULL, "\n");
+		}
+		args[x] = NULL;
 
-        // Create a child process to execute the command
-        pid = fork();
-        if (pid == -1)
-        {
-            perror("Error forking");
-            continue;
-        }
+		pid = fork();
+		if (pid == -1)
+		{
+			perror("Fork Error");
+			continue;
+		}
 
-        if (pid == 0)
-        {
-            // Execute the command
-            if (execve(args[0], args, NULL) == -1)
-            {
-                perror("Error executing command");
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            wait(&status);
-        }
-    }
+		if (pid == 0)
+		{
+			if (execve(args[0], args, NULL) == -1)
+			{
+				perror("Error Executing command");
+				exit(EXIT_FAILURE);
+			}
+		}
+		else
+		{
+			wait(&status);
+		}
+	}
 
-    free(line);
-    return (0);
+	free(line);
+	return (0);
 }
- 
+
