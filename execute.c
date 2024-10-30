6c4e28258b8e421748cb8ee52d9047e50786538d
PhetlaDaCoder <hsphetla@gmail.com>
2024-10-30 07:31:42 -0700

    fixed

--- execute.c
+++ execute.c
@@ -17,17 +17,20 @@ int exec(char **argv)
 	{
 		command = argv[0];
 		command_path = location(command);
+
 		if (command_path == NULL || *command_path == '\0')
 		{
 			error("command not found", command);
 			return (-1);
 		}
+
 		pid = fork();
 		if (pid == -1)
 		{
 			perror("Fork failed");
 			return (-1);
 		}
+
 		if (pid == 0)
 		{
 			if (execve(command_path, argv, env) == -1)
@@ -39,13 +42,16 @@ int exec(char **argv)
 		else
 		{
 			waitpid(pid, &status, 0);
-			if (WIFEXITED(status) && (WEXITSTATUS(status) == 127 ||
-						WEXITSTATUS(status) == 126))
+			if (WIFEXITED(status))
 			{
-				error("command not found", command);
+				int exit_status = WEXITSTATUS(status);
+				if (exit_status == 127 || exit_status == 126)
+				{
+					error("command not found", command);
+				}
+				return (exit_status);
 			}
-			return (WEXITSTATUS(status));
 		}
-		return (-1);
 	}
+	return (-1);
 }
